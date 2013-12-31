9c951db9116582e9dfe16cb62eb0228285c0b88a
andi34 <skate4life@gmx.de>
2013-12-31 14:41:36 +0100

    Place "init.recovery.espresso.rc" inside Recovery-Ramdisk
    Place "init.recovery.espresso.rc" inside Recovery-Ramdisk to fix
    Touch-Orientation in Recovery

--- p31xx-common.mk
+++ p31xx-common.mk
@@ -37,7 +37,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.espresso.rc:root/init.espresso.rc \
     $(LOCAL_PATH)/rootdir/ueventd.espresso.rc:root/ueventd.espresso.rc \
     $(LOCAL_PATH)/rootdir/fstab.espresso:root/fstab.espresso \
-    $(LOCAL_PATH)/rootdir/system/etc/init.d/01rotate:system/etc/init.d/01rotate
+    $(LOCAL_PATH)/rootdir/system/etc/init.d/01rotate:system/etc/init.d/01rotate \
+    $(LOCAL_PATH)/recovery/root/init.recovery.espresso.rc:recovery/root/init.recovery.espresso.rc
 
 # GPS
 PRODUCT_COPY_FILES += \
