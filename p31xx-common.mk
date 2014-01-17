05b0dea6586e383d6072af9439a0c9829c9ead2a
Andreas B <skate4life@gmx.de>
2014-01-18 00:38:06 +0100

    [2.2/2]p3100: move non-common configs to individual device tree
    ... forgot to add with [2/2]p3100: move non-common configs to individual
    device tree
    
    Change-Id: I9e47fab9ae0677346b7ded188236b8db07387d3f

--- p31xx-common.mk
+++ p31xx-common.mk
@@ -113,7 +113,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.disable_scissor_opt=true
-	
+
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
